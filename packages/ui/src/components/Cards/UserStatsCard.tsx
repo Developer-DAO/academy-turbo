a2c5243a6ad1642d9d28b2b74727bf151da9e752
Tony Stark <57156589+okhaimie-dev@users.noreply.github.com>
2023-09-06 14:18:08 -0500

    fix: user stats card minor update

--- packages/ui/src/components/Cards/UserStatsCard.tsx
+++ packages/ui/src/components/Cards/UserStatsCard.tsx
@@ -6,40 +6,42 @@ import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 export const UserStatsCard: React.FC = () => {
   return (
     <Card className="profile-card">
-      <CardHeader>
-        <CardTitle className="text-3xl">Stats</CardTitle>
-      </CardHeader>
-      <CardContent className="text-xl font-semibold">
-        <div className="flex flex-row">
-          <div className="mr-12">
-            <div className="flex flex-row items-center py-3">
-              <div className="mr-4">
-                <Icons.in_progress />
+      <div className="p-5 pb-10">
+        <CardHeader>
+          <CardTitle className="text-3xl">Stats</CardTitle>
+        </CardHeader>
+        <CardContent className="text-xl font-semibold">
+          <div className="flex flex-row justify-between">
+            <div className="mr-12">
+              <div className="flex flex-row items-center py-3">
+                <div className="mr-4">
+                  <Icons.in_progress />
+                </div>
+                <h3>In Progress</h3>
               </div>
-              <h3>In Progress</h3>
+              <div className="flex flex-row items-center py-3">
+                <div className="mr-4">
+                  <Icons.checkmark />
+                </div>
+                <h3>Completed</h3>
+              </div>
+              <div className="flex flex-row items-center py-3">
+                <div className="mr-4">
+                  <Icons.nft_mint />
+                </div>
+                <h3>NFT&apos;s Minted</h3>
+              </div>
             </div>
-            <div className="flex flex-row items-center py-3">
-              <div className="mr-4">
-                <Icons.checkmark />
-              </div>
-              <h3>Completed</h3>
-            </div>
-            <div className="flex flex-row items-center py-3">
-              <div className="mr-4">
-                <Icons.nft_mint />
-              </div>
-              <h3>NFT&apos;s Minted</h3>
+            <div>
+              <ul className="text-right">
+                <li className="py-3">5</li>
+                <li className="py-3">32</li>
+                <li className="py-3">21</li>
+              </ul>
             </div>
           </div>
-          <div>
-            <ul className="text-right">
-              <li className="py-3">5</li>
-              <li className="py-3">32</li>
-              <li className="py-3">21</li>
-            </ul>
-          </div>
-        </div>
-      </CardContent>
+        </CardContent>
+      </div>
     </Card>
   );
 };
