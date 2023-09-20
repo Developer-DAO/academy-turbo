1f2b9d311436af66132cd0c35baefd1546a2b7d6
Marcos Iglesias <markkos89@gmail.com>
2023-09-20 13:42:18 -0300

    feat: fix api type and queryClient options

--- apps/academy/src/utils/api.ts
+++ apps/academy/src/utils/api.ts
@@ -1,6 +1,7 @@
 import { httpBatchLink, loggerLink } from "@trpc/client";
 import { type CreateTRPCNext, createTRPCNext } from "@trpc/next";
 import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
+import type { NextPageContext } from "next";
 
 import type { AppRouter } from "@/server/api/root";
 import { transformer } from "@/server/transformer";
@@ -12,25 +13,33 @@ const getBaseUrl = () => {
   return `http://localhost:3000`; // dev SSR should use localhost
 };
 
-// TODO: DEV: enhance the `api` type avoiding the need to cast to any
-export const api: CreateTRPCNext<AppRouter, any, any> = createTRPCNext<AppRouter>({
-  config() {
-    return {
-      transformer,
-      links: [
-        loggerLink({
-          enabled: (opts) =>
-            process.env.NODE_ENV === "development" ||
-            (opts.direction === "down" && opts.result instanceof Error),
-        }),
-        httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`,
-        }),
-      ],
-    };
-  },
-  ssr: false,
-});
+export const api: CreateTRPCNext<AppRouter, NextPageContext, DecoratorContext> =
+  createTRPCNext<AppRouter>({
+    config() {
+      return {
+        queryClientConfig: {
+          defaultOptions: {
+            queries: {
+              refetchOnMount: false,
+              refetchOnWindowFocus: false,
+            },
+          },
+        },
+        transformer,
+        links: [
+          loggerLink({
+            enabled: (opts) =>
+              process.env.NODE_ENV === "development" ||
+              (opts.direction === "down" && opts.result instanceof Error),
+          }),
+          httpBatchLink({
+            url: `${getBaseUrl()}/api/trpc`,
+          }),
+        ],
+      };
+    },
+    ssr: false,
+  });
 
 /**
  * Inference helpers for input types
