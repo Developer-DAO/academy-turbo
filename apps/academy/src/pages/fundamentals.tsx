296b0d071ca9135160b3a96ccb31da263adbac16
Tony Stark <oktonystark8@gmail.com>
2023-10-26 22:08:08 -0500

    feat: fundamentals improve

--- apps/academy/src/pages/fundamentals.tsx
+++ apps/academy/src/pages/fundamentals.tsx
@@ -24,26 +24,26 @@ const sampleMenus: NavItem[] = [
 
 const Fundamentals: FC = () => {
   return (
-    <div className="flex h-screen w-full flex-col space-y-2.5 bg-black sm:flex-row sm:space-y-0">
-      <div className="fixed inset-y-0 flex w-1/2 flex-1 flex-col items-center justify-between bg-[url('/fundamental-bg.jpeg')] bg-cover bg-no-repeat object-center">
+    <div className="flex h-screen w-full flex-col bg-black lg:flex-row">
+      <div className="flex h-full min-h-screen flex-1 flex-col items-center justify-between bg-[url('/fundamental-bg.jpeg')] bg-cover bg-no-repeat object-center lg:fixed lg:inset-y-0 lg:w-1/2">
         <div className="mt-10">
           <TopBar menus={sampleMenus} />
         </div>
         <div>
-          <h2>Fundamentals</h2>
+          <h2 className="text-5xl text-white">Fundamentals</h2>
         </div>
         <div>
           <p className="text-3xl font-bold text-white">Nail the basics and then take on a track.</p>
         </div>
         <div />
       </div>
-      <div className="fixed right-0 top-0 h-screen w-1/2">
+      <div className="lg:fixed lg:right-0 lg:top-0 lg:h-screen lg:w-1/2">
         <div className="flex max-h-screen w-full flex-1 flex-col space-y-10 overflow-y-scroll bg-black px-8">
           <div className="mt-10 flex h-fit w-full justify-end">
             <ConnectButton />
           </div>
           <div className="flex w-full justify-center px-8">
-            <div className="grid w-fit grid-cols-2 justify-center gap-x-10 gap-y-8 pb-8">
+            <div className="grid w-fit grid-cols-1 justify-center gap-x-10 gap-y-8 pb-8 xl:grid-cols-2">
               <TrackCard imgSrc="/eth_fam.jpg" />
               <TrackCard imgSrc="/eth_fam.jpg" />
               <TrackCard imgSrc="/eth_fam.jpg" />
