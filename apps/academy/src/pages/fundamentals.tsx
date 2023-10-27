3d4516351a01f3bf2855b5c0e6d28496786efca8
Tony Stark <oktonystark8@gmail.com>
2023-10-26 22:32:01 -0500

    feat: fundamentals review

--- apps/academy/src/pages/fundamentals.tsx
+++ apps/academy/src/pages/fundamentals.tsx
@@ -7,7 +7,7 @@ import { ConnectButton } from "@/components/ConnectButton";
 const sampleMenus: NavItem[] = [
   {
     name: "Fundamentals",
-    href: "/",
+    href: "/fundamentals",
     icon: "clarity_blocks",
   },
   {
@@ -23,6 +23,10 @@ const sampleMenus: NavItem[] = [
 ];
 
 const Fundamentals: FC = () => {
+  const trackCards = Array.from({ length: 10 }).map(() => ({
+    imgSrc: "/eth_fam.png",
+  }));
+
   return (
     <div className="flex h-screen w-full flex-col bg-black lg:flex-row">
       <div className="flex h-full min-h-screen flex-1 flex-col items-center justify-between bg-[url('/fundamental-bg.jpeg')] bg-cover bg-no-repeat object-center lg:fixed lg:inset-y-0 lg:w-1/2">
@@ -30,7 +34,7 @@ const Fundamentals: FC = () => {
           <TopBar menus={sampleMenus} />
         </div>
         <div>
-          <h2 className="text-5xl text-white">Fundamentals</h2>
+          <h2 className="text-bttf text-5xl text-white">Fundamentals</h2>
         </div>
         <div>
           <p className="text-3xl font-bold text-white">Nail the basics and then take on a track.</p>
@@ -44,15 +48,9 @@ const Fundamentals: FC = () => {
           </div>
           <div className="flex w-full justify-center px-8">
             <div className="grid w-fit grid-cols-1 justify-center gap-x-10 gap-y-8 pb-8 xl:grid-cols-2">
-              <TrackCard imgSrc="/eth_fam.jpg" />
-              <TrackCard imgSrc="/eth_fam.jpg" />
-              <TrackCard imgSrc="/eth_fam.jpg" />
-              <TrackCard imgSrc="/eth_fam.jpg" />
-              <TrackCard imgSrc="/eth_fam.jpg" />
-              <TrackCard imgSrc="/eth_fam.jpg" />
-              <TrackCard imgSrc="/eth_fam.jpg" />
-              <TrackCard imgSrc="/eth_fam.jpg" />
-              <TrackCard imgSrc="/eth_fam.jpg" />
+              {trackCards.map((trackCard, index) => (
+                <TrackCard key={index} imgSrc={trackCard.imgSrc} />
+              ))}
             </div>
           </div>
         </div>
