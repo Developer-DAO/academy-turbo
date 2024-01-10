0953b3e0a57cc63d5aa97941c3e56b06bb11a85f
Marcos Iglesias <markkos89@gmail.com>
2024-01-10 01:00:20 -0300

    feat: fixed footer links among other small stuff

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -13,4 +13,5 @@ export * from "./ui/badge";
 export * from "./ui/dialog";
 export * from "./ui/drawer";
 export * from "./ui/toaster";
+export * from "./ui/tooltip";
 export * from "./ui/use-toast";
