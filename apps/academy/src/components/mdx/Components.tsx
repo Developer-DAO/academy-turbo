63ff0314634e5f731ad5030ea338668fbdf48424
wolovim <3621728+wolovim@users.noreply.github.com>
2024-01-21 21:23:46 -0500

    feat: mdx baseline styles

--- apps/academy/src/components/mdx/Components.tsx
+++ apps/academy/src/components/mdx/Components.tsx
@@ -27,15 +27,15 @@ const Components = {
     }
 
     // return <Code fontSize="md" wordBreak="break-all" {...props} />;
-    return <div className="text-md break-all" {...props} />;
+    return <span className="text-md inline-code break-all" {...props} />;
   },
-  h1: (props: any) => <h1 apply="mdx.h1" className="text-4xl" {...props} />,
-  h2: (props: any) => <h2 apply="mdx.h2" className="text-3xl" {...props} />,
-  h3: (props: any) => <h3 apply="mdx.h3" className="text-2xl" {...props} />,
-  h4: (props: any) => <h4 apply="mdx.h4" className="text-xl" {...props} />,
+  h1: (props: any) => <h1 apply="mdx.h1" className="h1" {...props} />,
+  h2: (props: any) => <h2 apply="mdx.h2" className="h2" {...props} />,
+  h3: (props: any) => <h3 apply="mdx.h3" className="h3" {...props} />,
+  h4: (props: any) => <h4 apply="mdx.h4" className="h4" {...props} />,
   p: (props: any) => <p apply="mdx.p" className="text-xl" {...props} />,
   a: (props: any) => <a apply="mdx.a" {...props} />,
-  ul: (props: any) => <ul apply="mdx.ul" className="text-xl" {...props} />,
+  ul: (props: any) => <ul apply="mdx.ul" className="ul text-xl" {...props} />,
   img: (props: any) => <img apply="mdx.image" className="m-0" alt="" {...props} />,
   SideDrawer,
   Callout,
