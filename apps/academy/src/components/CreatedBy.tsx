30e72a66c8cfa5676b3c99d0c07797d628ae9d75
wolovim <3621728+wolovim@users.noreply.github.com>
2024-01-31 11:20:41 -0500

    fix: lesson content container styling

--- apps/academy/src/components/CreatedBy.tsx
+++ apps/academy/src/components/CreatedBy.tsx
@@ -2,9 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "ui/components/ui/avatar";
 
 interface CreatedByProps {
   author: string;
-  authorPosition: string;
   authorTwitter: string;
-  createdDate: string;
 }
 
 export default function CreatedBy({ author, authorTwitter }: CreatedByProps) {
@@ -23,7 +21,7 @@ export default function CreatedBy({ author, authorTwitter }: CreatedByProps) {
   ));
 
   return (
-    <section className="grid place-items-start pt-6 text-sm lg:mx-16 lg:text-xl">
+    <section className="grid place-items-start pt-6 text-sm lg:text-xl">
       <div className="flex w-full">
         <p className="text-left">Created by:</p>
         <Avatar className="ml-6 h-16 w-16">
@@ -37,24 +35,6 @@ export default function CreatedBy({ author, authorTwitter }: CreatedByProps) {
           </article>
         </div>
       </div>
-
-      {/* <div className="mt-4">
-        <div className="mr-2 inline-flex h-8 w-14 items-center justify-center gap-2 rounded-3xl border border-neutral-600 border-opacity-30 bg-yellow-400 bg-opacity-40 p-2 backdrop-blur-md">
-          <div className="font-['Clash Display'] text-center text-sm font-semibold text-white">
-            Web3
-          </div>
-        </div>
-        <div className="mr-2 inline-flex h-8 w-14 items-center justify-center gap-2 rounded-3xl border border-neutral-600 border-opacity-30 bg-red-600 bg-opacity-30 p-2 backdrop-blur-md">
-          <div className="font-['Clash Display'] text-center text-sm font-semibold text-white">
-            Eth
-          </div>
-        </div>
-        <div className="inline-flex h-8 w-20 items-center justify-center gap-2 rounded-3xl border border-neutral-600 border-opacity-30 bg-cyan-400 bg-opacity-30 p-2 backdrop-blur-md">
-          <div className="font-['Clash Display'] text-center text-sm font-semibold text-white">
-            Beginner
-          </div>
-        </div>
-      </div> */}
     </section>
   );
 }
