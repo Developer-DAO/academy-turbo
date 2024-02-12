10df76eb7b7b2cdd44eba621e526053d16e331b3
Marcos Iglesias <markkos89@gmail.com>
2024-02-12 12:35:08 -0300

    feat: changing quizCompleteModals content

--- apps/academy/src/components/mdx/QuizCompletedModals.tsx
+++ apps/academy/src/components/mdx/QuizCompletedModals.tsx
@@ -11,7 +11,6 @@ import {
   DialogOverlay,
   DialogTitle,
   DialogTrigger,
-  // useToast,
 } from "ui";
 
 export interface QuizProps {
@@ -24,11 +23,12 @@ export type Answers = Record<string, number[]>;
 
 const QuizCompletedModals = (props: QuizProps): JSX.Element => {
   const [showDialog, setShowDialog] = useState(false);
-  const [showKeepGoingModal, setShowKeepGoingModal] = useState(false);
+  // const [showKeepGoingModal, setShowKeepGoingModal] = useState(false);
 
-  const handleLessonDoneClick = () => {
-    setShowKeepGoingModal(true);
-  };
+  // const handleLessonDoneClick = () => {
+  // setShowKeepGoingModal(true);
+  // setShowDialog(false);
+  // };
 
   const handleClose = () => {
     setShowDialog(false);
@@ -57,22 +57,22 @@ const QuizCompletedModals = (props: QuizProps): JSX.Element => {
             >
               <DialogTrigger className="w-full text-right text-[#44AF96]">X</DialogTrigger>
               <div className="w-full text-center">
-                {!showKeepGoingModal ? (
+                {/*  {!showKeepGoingModal ? (
                   <span className="font-clash-display w-full text-center text-2xl font-bold leading-8 text-white">
                     Quiz complete!
                   </span>
-                ) : (
-                  <span className="font-clash-display w-full text-center text-2xl font-bold leading-8 text-white">
-                    Nice!
-                  </span>
-                )}
+                ) : ( */}
+                <span className="font-clash-display w-full text-center text-2xl font-bold leading-8 text-white">
+                  Nice!
+                </span>
+                {/* )} */}
               </div>
             </DialogTitle>
           </DialogHeader>
 
           <DialogDescription className="mb-[3rem] h-[35rem] max-h-[35rem] w-full rounded-md bg-[#242424] pb-5 lg:mb-0">
             <div className="flex flex-col gap-4 lg:p-0">
-              {!showKeepGoingModal ? (
+              {/*  {!showKeepGoingModal ? (
                 <div className="max-h-64 w-fit lg:mt-11 lg:h-96 lg:w-full">
                   <div
                     className={`font-clash-display w-full cursor-pointer rounded-3xl p-3 text-center font-bold text-[#F9F9F9]`}
@@ -98,60 +98,34 @@ const QuizCompletedModals = (props: QuizProps): JSX.Element => {
                     </ButtonRaw>
                   </div>
                 </div>
-              ) : (
-                <div className="h-64 w-fit lg:mt-11 lg:h-96 lg:w-full">
-                  <div
-                    className={`font-clash-display w-full cursor-pointer rounded-3xl p-3 text-center font-bold text-[#F9F9F9]`}
-                  >
-                    <h1 className="font-clash-display mb-11 text-3xl lg:text-[26px]">
-                      Keep going!
-                    </h1>
-                    <Image
-                      src={"/happy_face.png"}
-                      alt="happy_face_icon"
-                      width={100}
-                      height={100}
-                      className="mx-auto mb-16"
-                    />
-                    <p className="mb-20 text-base font-normal leading-5 text-[#FFFFFF] lg:mb-10 lg:text-2xl">
-                      {`You've just completed ${props.actualLessonTitle}!`}
-                    </p>
-                    <NextLink href={props.nextLessonURLPath}>
-                      <ButtonRaw className="font-future h-fit w-fit bg-[#44AF96] lg:h-[4.125rem] lg:w-80 lg:min-w-[21rem] lg:text-base">
-                        {`NEXT: ${props.nextLessonTitle}`}
-                      </ButtonRaw>
-                    </NextLink>
-                  </div>
+              ) : ( */}
+              <div className="h-64 w-fit lg:mt-11 lg:h-96 lg:w-full">
+                <div
+                  className={`font-clash-display w-full cursor-pointer rounded-3xl p-3 text-center font-bold text-[#F9F9F9]`}
+                >
+                  <h1 className="font-clash-display mb-11 text-3xl lg:text-[26px]">Keep going!</h1>
+                  <Image
+                    src={"/happy_face.png"}
+                    alt="happy_face_icon"
+                    width={100}
+                    height={100}
+                    className="mx-auto mb-16"
+                  />
+                  <p className="mb-20 text-base font-normal leading-5 text-[#FFFFFF] lg:mb-10 lg:text-2xl">
+                    {`You've just completed ${props.actualLessonTitle}!`}
+                  </p>
+                  <NextLink href={props.nextLessonURLPath}>
+                    <ButtonRaw className="font-future h-fit w-fit bg-[#44AF96] lg:h-[4.125rem] lg:w-80 lg:min-w-[21rem] lg:text-base">
+                      {`NEXT: ${props.nextLessonTitle}`}
+                    </ButtonRaw>
+                  </NextLink>
                 </div>
-              )}
+              </div>
+              {/*  )} */}
             </div>
           </DialogDescription>
 
-          <DialogFooter>
-            {/* <div className="flex w-full">
-              <ButtonRaw
-                type="button"
-                className={`button-rounded mr-4 text-xs font-normal text-white lg:w-40 ${previousButtonVisibility()}`}
-                onClick={previousQuestion}
-              >
-                {"Previous"}
-              </ButtonRaw>
-              <ButtonRaw
-                type="button"
-                className={`button-rounded mr-4 w-32 text-xs font-normal text-white ${nextButtonVisibility()}`}
-                onClick={nextQuestion}
-              >
-                {"Next"}
-              </ButtonRaw>
-              <ButtonRaw
-                className="font-future w-32 rounded-3xl bg-[#636363] text-xs font-normal text-white"
-                onClick={submit}
-                disabled={Object.keys(answers).length !== quiz.questions.length}
-              >
-                {!quizzesAddIsLoading ? "Submit" : <Spinner />}
-              </ButtonRaw>
-            </div> */}
-          </DialogFooter>
+          <DialogFooter></DialogFooter>
         </DialogContent>
       </Dialog>
     </>
