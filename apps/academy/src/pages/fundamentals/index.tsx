fd56d090a976bb078e34a07cafd34d8c28106990
wolovim <3621728+wolovim@users.noreply.github.com>
2024-02-14 15:55:57 -0500

    fix: darker fundamentals img + centered

--- apps/academy/src/pages/fundamentals/index.tsx
+++ apps/academy/src/pages/fundamentals/index.tsx
@@ -22,10 +22,10 @@ const FundamentalsPage = () => {
     <div className="flex h-full w-full flex-col space-y-10 overflow-hidden bg-black lg:h-screen lg:max-h-screen lg:flex-row">
       <div
         className="flex h-full min-h-screen flex-1 flex-col items-center justify-between overflow-hidden 
-bg-[url('/bg_fundamentals.png')] bg-cover bg-no-repeat object-center pt-[300px]  lg:fixed lg:inset-y-0 lg:h-screen lg:w-1/2"
+bg-[url('/bg_fundamentals.png')] bg-cover bg-center bg-no-repeat object-center pt-[300px]  lg:fixed lg:inset-y-0 lg:h-screen lg:w-1/2"
       >
         <div>
-          <h2 className="text-bttf text-5xl text-white lg:text-6xl">Fundamentals</h2>
+          <h2 className="text-bttf text-3xl text-white lg:text-5xl">Fundamentals</h2>
         </div>
         <div className="flex justify-center">
           <p className="max-w-[275px] text-center text-xl font-bold text-white sm:max-w-xs md:max-w-full lg:text-2xl">
