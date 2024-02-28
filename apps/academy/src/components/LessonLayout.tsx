9bfdeab71dab436a826025df84b48f7dff1906e7
kempsterrrr <kempsterwilliam@gmail.com>
2024-02-28 17:54:39 -0600

    feat: lesson titles now showing

--- apps/academy/src/components/LessonLayout.tsx
+++ apps/academy/src/components/LessonLayout.tsx
@@ -1,5 +1,6 @@
 // import AboutCourse from "@/components/AboutCourse";
 import CreatedBy from "@/components/CreatedBy";
+import PageSeoLayout from "@/components/PageSeoLayout";
 
 interface LessonLayoutProps {
   children: React.ReactNode;
@@ -19,14 +20,16 @@ export default function LessonLayout({
   authorTwitter,
 }: LessonLayoutProps) {
   return (
-    <main className="px-10 pt-36 text-white lg:mx-auto lg:max-w-screen-lg lg:pt-44">
-      <section className="text-center">
-        <h1 className="font-future text-3xl lg:text-6xl">{lessonTitle}</h1>
-      </section>
-      <CreatedBy author={author} authorImage={authorImage} authorTwitter={authorTwitter} />
-      <div className="font-poppins pt-4 text-xl font-medium tracking-wider lg:mx-auto lg:max-w-screen-lg">
-        {children}
-      </div>
-    </main>
+    <PageSeoLayout title={lessonTitle} description="">
+      <main className="px-10 pt-36 text-white lg:mx-auto lg:max-w-screen-lg lg:pt-44">
+        <section className="text-center">
+          <h1 className="font-future text-3xl lg:text-6xl">{lessonTitle}</h1>
+        </section>
+        <CreatedBy author={author} authorImage={authorImage} authorTwitter={authorTwitter} />
+        <div className="font-poppins pt-4 text-xl font-medium tracking-wider lg:mx-auto lg:max-w-screen-lg">
+          {children}
+        </div>
+      </main>
+    </PageSeoLayout>
   );
 }
