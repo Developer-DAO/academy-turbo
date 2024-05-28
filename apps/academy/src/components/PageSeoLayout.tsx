04084e87eda601ccc604f80ecaf0c2948d9b6ad8
kempsterrrr <kempsterwilliam@gmail.com>
2024-05-28 21:18:28 +0100

    feat: adding tracks meta image

--- apps/academy/src/components/PageSeoLayout.tsx
+++ apps/academy/src/components/PageSeoLayout.tsx
@@ -4,12 +4,28 @@ interface PageSeoLayoutProps {
   title: string;
   description: string;
   children: React.ReactNode;
+  imgPath?: string;
 }
 
 const PageSeoLayout = ({ title, description, children }: PageSeoLayoutProps) => {
   return (
     <>
-      <NextSeo title={title} description={description} />
+      <NextSeo
+        title={title}
+        description={description}
+        // openGraph={{
+        //   title: title,
+        //   description: description,
+        //   images: [
+        //     {
+        //       url: imgPath
+        //         ? `https://${process.env["NEXT_PUBLIC_VERCEL_URL"]}/${imgPath}`
+        //         : imgPath,
+        //       alt: title,
+        //     },
+        //   ],
+        // }}
+      />
       {children}
     </>
   );
