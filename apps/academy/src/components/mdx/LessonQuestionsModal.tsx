b6628693d2370935490d41aca67459c455fdc366
wolovim <3621728+wolovim@users.noreply.github.com>
2024-05-29 13:01:41 -0400

    fix: lesson, quiz styling

--- apps/academy/src/components/mdx/LessonQuestionsModal.tsx
+++ apps/academy/src/components/mdx/LessonQuestionsModal.tsx
@@ -19,15 +19,15 @@ export default function LessonQuestionsModal({ buttonText, title, children }: Pr
   return (
     <Dialog>
       <DialogTrigger
-        className="font-future mx-auto my-3 flex w-fit rounded-3xl bg-[#721F79]
-          p-2 text-2xl font-normal text-white"
+        className="font-poppins mx-auto my-3 flex w-fit rounded-3xl bg-[#721F79]
+          p-2 px-4 text-2xl font-semibold text-white"
       >
         {buttonText}
       </DialogTrigger>
       <DialogOverlay />
       <DialogContent className="mb-32 mt-10 max-h-screen overflow-y-auto border-0 bg-[#1C1C1C] lg:min-w-[50%] ">
         <DialogHeader>
-          <DialogTitle className="font-clash-display m-2 text-2xl font-bold text-white">
+          <DialogTitle className="font-poppins m-2 text-2xl font-bold text-white">
             <div className="w-full text-right text-[#44AF96]">
               <DialogTrigger className="font-semibold	">X</DialogTrigger>
             </div>
