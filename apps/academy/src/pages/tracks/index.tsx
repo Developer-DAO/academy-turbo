b006b96302e880bdc96f14709b073ca4a8b8f242
kempsterrrr <kempsterwilliam@gmail.com>
2024-05-29 17:14:24 +0100

    feat: adding tracks meta image

--- apps/academy/src/pages/tracks/index.tsx
+++ apps/academy/src/pages/tracks/index.tsx
@@ -62,6 +62,17 @@ TracksPage.getLayout = function getLayout(page: ReactElement): ReactElement {
     <PageSeoLayout
       title="Web3 Learning Tracks"
       description="Our Web3 courses are designed to get you from 0 to 1 via project-based learning tracks."
+      openGraph={{
+        images: [
+          {
+            url:
+              process.env["NEXT_PUBLIC_VERCEL_URL"] !== undefined
+                ? `https://${process.env["NEXT_PUBLIC_VERCEL_URL"]}/meta-images/academy-web3-tracks.png`
+                : "/meta-images/academy-web3-tracks.png",
+            alt: "Web3 Learning Tracks",
+          },
+        ],
+      }}
     >
       {page}
     </PageSeoLayout>
